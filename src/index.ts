f3c942080813fc3ce5033af1a52c2316697f8c00
leticia2chen <n1076730@ntub.edu.tw>
2019-10-14 20:38:12 +0800

    homework

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 // 請介紹兩個字串方法跟數字方法
 
+var worldString = 'Hello, world';
+console.log(worldString.blink());  // 讓文字緩慢閃爍
+console.log(worldString.bold());  //  讓文字顯示粗體
+
+Number.MAX_VALUE  //可表示的最大正整數。
+Number.MIN_VALUE  //可表示的最小值，即最靠近0的正整數
 
 // 錢錢大於一個值，叫乾爹
 let money = 40000;
 let limit = 500000; // 限制值
-
+if (money>40000 ) {
+    console.log('爹');
+}
+else{
+    console.log('娘');
+}  
